b409eccacee8cd75d3e81b621f8cdc3f16a0cbde
Duy Nguyen <hoanduy27@gmail.com>
2021-08-22 21:05:41 +0700

    fix sign up

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -91,25 +91,25 @@ class Signup extends Component {
               <label className="col" htmlFor="username">Username</label>
               <div className="text-danger text-right col" hidden={this.state.validUsername}>Invalid username</div>
             </div>
-            <input type="username" name="username" className="form-control" id="username" aria-describedby="username" placeholder="No space, not blank"  maxLength="10" value={this.state.username} onChange={this.onChange} />
+            <input type="username" name="username" className="form-control" id="username" aria-describedby="username" placeholder="No space, not blank, < 10 characters"  maxLength="10" value={this.state.username} onChange={this.onChange} />
           </div>
           <div className="form-group">
             <div className="row">
               <label className="col" htmlFor="name">Name</label>
               <div className="text-danger text-right col" hidden={this.state.validName} maxLength="30">Invalid name</div>
             </div>
-            <input type="name" name="name" className="form-control" id="name" aria-describedby="name" placeholder="Name" value={this.state.name} onChange={this.onChange} />
+            <input type="name" name="name" className="form-control" id="name" aria-describedby="name" placeholder="Not blank, < 30 characters" value={this.state.name} onChange={this.onChange} />
           </div>
           <div className="form-group">
             <div className="row">
               <label htmlFor="password" className="col">Password</label>
               <div className="text-danger text-right col" hidden={this.state.validPassword}>Invalid password</div>
             </div>
-            <input type="password" name="password" className="form-control" id="password" placeholder="Password" value={this.state.password} onChange={this.onChange} />
+            <input type="password" name="password" className="form-control" id="password" placeholder="No space, not blank" value={this.state.password} onChange={this.onChange} />
           </div>
           <div className="form-group">
             <label htmlFor="confirmpassword">Re-enter your Password</label>
-            <input type="password" name="password2" className="form-control" id="confirmpassword" placeholder="Password again" value={this.state.password2} onChange={this.onChange} />
+            <input type="password" name="password2" className="form-control" id="confirmpassword" placeholder="Must match with the password above" value={this.state.password2} onChange={this.onChange} />
             <div className="text-danger" hidden={this.state.matchPassword}>Password does not match!</div>
           </div>
           <button type="button" className="btn btn-primary float-right" onClick={this.onSubmitSignup}>
