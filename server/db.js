a58c5e148ed218f86b665cf973f4a20d84c30449
Duy Nguyen <hoanduy27@gmail.com>
2021-08-22 14:28:08 +0700

    config db for heroku

--- server/db.js
+++ server/db.js
@@ -1,11 +1,18 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: "123",
+//     database: "notee",
+//     host: "localhost",
+//     port: 5432
+// });
+const DB_URL = "postgres://jwxnwipgpdhoer:fc5f5e51b3a51bc99b1bf6d6955c5abc8d6cdbb8d6980ac76b5bfa5a22203927@ec2-54-147-93-73.compute-1.amazonaws.com:5432/datrd6m6ekecfa"
 const pool = new Pool({
-    user: 'postgres',
-    password: "123",
-    database: "notee",
-    host: "localhost",
-    port: 5432
+    connectionString: DB_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
